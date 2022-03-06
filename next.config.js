e340179e289f974b00f07125205523be401b274b
Nick Lima <nick.lima.wp@gmail.com>
2022-03-06 13:59:33 -0300

    ⚡️ adding formats / device size to next img loader

--- next.config.js
+++ next.config.js
@@ -6,13 +6,10 @@ const nextConfig = {
     styledComponents: true,
   },
   images: {
+    deviceSizes: [320, 768, 1204, 1200, 1330, 1920, 2048, 3840],
+    formats: ["image/avif", "image/webp"],
     loader: "default",
-    domains: [
-      "localhost",
-      "herokuapp.com",
-      "res.cloudinary.com",
-      "cloudinary.com",
-    ],
+    domains: ["localhost", "res.cloudinary.com", "cloudinary.com"],
   },
 }
 
