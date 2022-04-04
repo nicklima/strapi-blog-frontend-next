a0943dab0f2f9e4882369e66c2e7ac83a2c42e8f
Nick Lima <nick.lima.wp@gmail.com>
2022-04-04 09:05:56 -0300

    🐛 fixing css render bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react"
+import { ServerStyleSheet } from "styled-components"
 import Document, {
   Html,
   Head,
@@ -9,8 +10,29 @@ import Document, {
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return initialProps
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render(): ReactElement {
