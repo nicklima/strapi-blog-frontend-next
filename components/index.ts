c60dd1fff6f7cf2e031ebdca61412937bdb9e574
Nick Lima <nick.lima.wp@gmail.com>
2022-09-13 18:56:29 -0300

    importing/exporting components

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Container } from "./Container"
+export { default as NavItem } from "./NavItem"
+export { default as NavLogo } from "./NavLogo"
+export { default as NavTop } from "./NavTop"
