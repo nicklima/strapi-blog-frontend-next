5ffb2239955d13c8f1926d077b7277bac45dbecc
Nick Lima <nick.lima.wp@gmail.com>
2022-09-14 12:08:05 -0300

    set IStrapiImage on get media

--- lib/media.ts
+++ lib/media.ts
@@ -1,7 +1,7 @@
 import { getStrapiURL } from "lib"
-import { StrapiImage } from "strapi-data"
+import { IStrapiImage } from "strapi-data"
 
-export const getStrapiMedia = (media: StrapiImage) => {
+export const getStrapiMedia = (media: IStrapiImage) => {
   const { url } = media.data.attributes
   const imageUrl = url.startsWith("/") ? getStrapiURL(url) : url
   return imageUrl
